06bb7528d05d6b9d61a6ea5182e7b694cac28be4
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 06:56:19 +0700

    Create AQI.php

---
+++ AQI.php
@@ -0,0 +1,26 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "api.airvisual.com/v2/countries?key={{c97b1c11-d68f-4acc-9ac9-13f8673e2844}}",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => false,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+  echo "OK";
+} ?>
