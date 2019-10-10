9b5ae21354caa2b08b5a3e220090bd5b25729ffa
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-10 21:56:33 +0700

    Update ConnectLineForensicMNRH.php

--- ConnectLineForensicMNRH.php
+++ ConnectLineForensicMNRH.php
@@ -7,4 +7,14 @@ $password = $url["e8ba9c70"];
 $db = substr($url["heroku_04736a2bbd78072"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+if (mysqli_connect_errno())
+	{
+		echo "Database Connect Failed : " . mysqli_connect_error();
+	}
+	else
+	{
+		echo "Database Connected.";
+	}
+
+	mysqli_close($conn);
 ?>
