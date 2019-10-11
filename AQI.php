acc06bbe5df4b15b84d0af123b183ed5ac44bf21
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 10:50:44 +0700

    Update AQI.php

--- AQI.php
+++ AQI.php
@@ -46,8 +46,8 @@ echo $arr[data]['country'];
 echo $arr[data]['state'];  
 echo $arr[data]['city']; 
 echo $arr[data]['location']['coordinates'];
-echo $arr[current]['weather']['ts'];
-  echo $arr[current]['weather']['tp'];
+echo $arr[current][weather]['ts'];
+  echo $arr[current][weather]['tp'];
 
   
 } 
