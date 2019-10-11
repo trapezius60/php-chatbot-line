2354f63506976747e095944d3b7abb9297cfa06a
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 14:09:17 +0700

    Update RESTAPI.php

--- RESTAPI.php
+++ RESTAPI.php
@@ -1,6 +1,12 @@
 <?php
 include "httpful.phar";
 $uri = "https://maps.googleapis.com/maps/api/geocode/json?address=Bangkok";
+
 $response = \Httpful\Request::get($uri)->send();
 echo $response;
+
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  https://automl.googleapis.com/v1beta1/projects/wounddx/locations/us-central1/models/ICN6670781548460833353:predict -d @request.json
+
 ?>
