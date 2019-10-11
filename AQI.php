8f63840ae02f72b4d1813a50ab51d2793aa32a1d
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 11:18:49 +0700

    Update AQI.php

--- AQI.php
+++ AQI.php
@@ -40,7 +40,7 @@ if ($err) {
   echo "cURL Error #:" . $err;
 } else {
 // Decode JSON data to PHP associative array
-$arr = json_decode($response, true);
+//$arr = json_decode($response, true);
 
 //echo $arr[data]['country']; 
 //echo $arr[data]['state'];  
