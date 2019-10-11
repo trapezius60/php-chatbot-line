d17b6c3784e3ea45c630efd2c053fc1424664981
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 09:03:31 +0700

    Update AQI.php

--- AQI.php
+++ AQI.php
@@ -1,20 +1,24 @@
 <?php
+
 // List country
 //api.airvisual.com/v2/countries?key=c97b1c11-d68f-4acc-9ac9-13f8673e2844
 //List Country & State
 //api.airvisual.com/v2/cities?state=Chiangmai&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844
 //api.airvisual.com/v2/cities?state=Chiang%20Mai&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844
-//api.airvisual.com/v2/city?city=Nakhon Ratchasima&state=Nakhon%20Ratchasima&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844
-// will return >> {"status":"success","data":[{"city":"Nakhon Ratchasima"}]}
+
+// will return >> {"status":"success","data":[{"city":"Chiang Mai"},{"city":"Hang Dong"},{"city":"San Sai"}]}
+//             >>{"status":"success","data":[{"city":"Nakhon Ratchasima"}]}
 //Specific data in city
 //api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key={{YOUR_API_KEY}}
-// LINK documentation >> https://documenter.getpostman.com/view/507654/airvisual-api/2Fvvgg?version=latest
+//api.airvisual.com/v2/city?city=Nakhon Ratchasima&state=Nakhon%20Ratchasima&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844
 
-// key = c97b1c11-d68f-4acc-9ac9-13f8673e2844 (แทน {{YOUR_API_KEY}})
+// LINK documentation >> https://documenter.getpostman.com/view/507654/airvisual-api/2Fvvgg?version=latest
+//                    >>   key = c97b1c11-d68f-4acc-9ac9-13f8673e2844 (แทน {{YOUR_API_KEY}})
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "api.airvisual.com/v2/cities?state=Chiang%20Mai&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844",
+  CURLOPT_URL => "api.airvisual.com/v2/city?city=Chiang Mai&state=Chiang%20Mai&country=Thailand&key=c97b1c11-d68f-4acc-9ac9-13f8673e2844",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
