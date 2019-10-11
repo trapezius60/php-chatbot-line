88a0bd55551aae86f2bc2cdbffcb18ca28656aba
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-11 14:53:10 +0700

    Update RESTAPI.php

--- RESTAPI.php
+++ RESTAPI.php
@@ -1,12 +1,15 @@
 <?php
 include "httpful.phar";
-$uri = "https://maps.googleapis.com/maps/api/geocode/json?address=Bangkok";
+$uri = "https://automl.googleapis.com/v1beta1/projects/wounddx/locations/us-central1/models/ICN6670781548460833353:predict -d @request.json";
 
-$response = \Httpful\Request::get($uri)->send();
+$response = \Httpful\Request::get($uri)
+ ->expectsJson()
+ ->withXTrivialHeader('Authorization: Bearer $(54e358213496c4520f739ec028a2fd266c137b6c)')
+    ->send();
 echo $response;
 
-curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
-  https://automl.googleapis.com/v1beta1/projects/wounddx/locations/us-central1/models/ICN6670781548460833353:predict -d @request.json
+//curl -X POST -H "Content-Type: application/json" \
+  //-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+ // https://automl.googleapis.com/v1beta1/projects/wounddx/locations/us-central1/models/ICN6670781548460833353:predict -d @request.json
 
 ?>
