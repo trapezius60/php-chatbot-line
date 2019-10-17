8b8659b146c71c7152ed731ee5ed8d68243af7dc
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-17 11:57:22 +0700

    Update and rename AQI.js to AQI.html

--- AQI.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-  method: "POST",
-  url: "AQI.php",  //php page link here
-  data: { date: Now()}  // send any data to php page if needed
-})
-  .done(function( msg ) {   //msg contains the response from php page...
-    alert( "Data Saved: " + msg );   //use msg to update the page without refresh
-  });
