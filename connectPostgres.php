b6a067581e7dcc96b0914cc35b0e9a3d63d7308f
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-27 05:44:41 +0700

    Update connectPostgres.php

--- connectPostgres.php
+++ connectPostgres.php
@@ -14,17 +14,7 @@ catch(PDOException $pe)
 {
 	die('Connection error, because: ' .$pe->getMessage());
 
-  // $db = "dbname=dc4tc15epqk7fb host=ec2-174-129-253-101.compute-1.amazonaws.com port=5432 user=achmkoobsocawj password=6a61be4049409369bd7ce5457fdd83728f0e2880b08601deb2c1b15e11e6dec0 sslmode=require";
-
-
- //  if (!$db) 
- //  {
- //    echo "Database connection failed.";
-   }
- //  else 
-   {
-  //   echo "Database connection success.";
-   }
+}
 $query = 'CREATE TABLE mytable (
     id SERIAL,
     facebookid BIGSERIAL,
