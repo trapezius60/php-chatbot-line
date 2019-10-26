3a66b56a991f3bd3e0d417e40f8704b5ceba764e
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-26 22:16:42 +0700

    Update connectPostgres.php

--- connectPostgres.php
+++ connectPostgres.php
@@ -1,11 +1,16 @@
 <?php
-$db = parse_url(getenv("postgres://achmkoobsocawj:6a61be4049409369bd7ce5457fdd83728f0e2880b08601deb2c1b15e11e6dec0@ec2-174-129-253-101.compute-1.amazonaws.com:5432/dc4tc15epqk7fb"));
-$db["path"] = ltrim($db["path"], "/");
 
-CREATE TABLE employees (
-    employee_id SERIAL,
-    last_name VARCHAR(30),
-    first_name VARCHAR(30),
-    title VARCHAR(50)
-);
+   $con = "dbname=dc4tc15epqk7fb host=ec2-174-129-253-101.compute-1.amazonaws.com port=5432 user=achmkoobsocawj password=6a61be4049409369bd7ce5457fdd83728f0e2880b08601deb2c1b15e11e6dec0 sslmode=require";
+
+
+   if (!$con) 
+   {
+     echo "Database connection failed.";
+   }
+   else 
+   {
+     echo "Database connection success.";
+   }
+
 ?>
+
