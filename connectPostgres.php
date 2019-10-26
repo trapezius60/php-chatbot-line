942351fedb087530f9c6e6b1ffef069ef0ea40d9
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-27 05:34:11 +0700

    Update connectPostgres.php

--- connectPostgres.php
+++ connectPostgres.php
@@ -1,9 +1,9 @@
 <?php
 
-   $con = "dbname=dc4tc15epqk7fb host=ec2-174-129-253-101.compute-1.amazonaws.com port=5432 user=achmkoobsocawj password=6a61be4049409369bd7ce5457fdd83728f0e2880b08601deb2c1b15e11e6dec0 sslmode=require";
+   $db = "dbname=dc4tc15epqk7fb host=ec2-174-129-253-101.compute-1.amazonaws.com port=5432 user=achmkoobsocawj password=6a61be4049409369bd7ce5457fdd83728f0e2880b08601deb2c1b15e11e6dec0 sslmode=require";
 
 
-   if (!$con) 
+   if (!$db) 
    {
      echo "Database connection failed.";
    }
@@ -11,6 +11,17 @@
    {
      echo "Database connection success.";
    }
+$query = 'CREATE TABLE mytable (
+    id SERIAL,
+    facebookid BIGSERIAL,
+    mytext TEXT,
+    inserted TIMESTAMP
+);';
+$db->query($query);
 
+$query = 'INSERT INTO mytable (facebookid,mytext,inserted)'
+    . ' VALUES (1603196280,"test",now());';
+$db->query($query);
+var_dump($db->errorInfo());
 ?>
 
