c540a0ab50da3c98ce2e0fa3e56163ba6d1eddb2
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2019-10-27 16:31:18 +0700

    Update app.php

--- GSAPI/app.php
+++ GSAPI/app.php
@@ -1,12 +1,12 @@
 <?php
 setApplicationName(APPLICATION_NAME);
 $client->setScopes(SCOPES);
-$client->setAuthConfig(CLIENT_SECRET_PATH);
+$client->setAuthConfig(GSAPI/sheets_api_secret.json);
 $client->setAccessToken(460d4c42098fe914bbf0949172fd34bb5ea48d2b);
 
 $service = new Google_Service_Sheets($client);
 
-$sheetInfo = $service->spreadsheets->get(SHEET_ID)->getProperties();
+$sheetInfo = $service->spreadsheets->get(1HG3VgbcLPzKL59wkyCb_OckHhe_eqP-mG4XfhAZO6iQ)->getProperties();
 
 print($sheetInfo['title']. PHP_EOL);
 ?>
