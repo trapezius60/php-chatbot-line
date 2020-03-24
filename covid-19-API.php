b653cc9340ad9ee9e79abb1d21da23495d2d9a93
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2020-03-24 10:01:14 +0700

    Update covid-19-API.php

--- covid-19-API.php
+++ covid-19-API.php
@@ -3,14 +3,16 @@
 $curl = curl_init();
  
 curl_setopt_array($curl, array(
-CURLOPT_URL => "https://opend.data.go.th/get-ckan/datastore_search?resource_id=93f74e67-6f76-4b25-8f5d-b485083100b6&api-key:tg2XPZo23UOjWWWpNpCRIejyQyvbIzKf",
+CURLOPT_URL => "https://opend.data.go.th/get-ckan/datastore_search?resource_id=93f74e67-6f76-4b25-8f5d-b485083100b6",
 CURLOPT_RETURNTRANSFER => true,
 CURLOPT_ENCODING => "",
 CURLOPT_MAXREDIRS => 10,
 CURLOPT_TIMEOUT => 30,
 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 CURLOPT_CUSTOMREQUEST => "GET",
-
+CURLOPT_HTTPHEADER => array(
+ "api-key:tg2XPZo23UOjWWWpNpCRIejyQyvbIzKf"
+ )
 ));
  
 $response = curl_exec($curl);
