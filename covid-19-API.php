0722fa9c43cfd79330c0c0b9bc2dedda99018cbd
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2020-03-24 10:11:50 +0700

    Update covid-19-API.php

--- covid-19-API.php
+++ covid-19-API.php
@@ -11,7 +11,7 @@ CURLOPT_TIMEOUT => 30,
 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 CURLOPT_CUSTOMREQUEST => "GET",
 CURLOPT_HTTPHEADER => array(
- "accept: application/json;charset=utf-8",
+ "Content-Type: application/json",
  "api-key: tg2XPZo23UOjWWWpNpCRIejyQyvbIzKf"
 ),
 ));
